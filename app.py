595d14f3cd171e95137573dd013cee799a48afdf
Lucas Tadeu Monteiro Guedes Fernandes Salomao <lucas.salomao@sp.senai.br>
2024-06-17 19:35:36 -0300

    update

--- app.py
+++ app.py
@@ -10,13 +10,14 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
+LOGO_URL_LARGE='https://upload.wikimedia.org/wikipedia/commons/8/8c/SENAI_S%C3%A3o_Paulo_logo.png'
+
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
+
 # read all pdf files and return text
-
-
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
@@ -44,8 +45,7 @@ def get_vector_store(chunks):
 
 def get_conversational_chain():
     prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Você é um especialista em educação profissional, que trabalha no Senai São Paulo, que orienta os professores a como usar a metodologia senai de educação profissional para elaborar planos de ensino, cronogramas e planos de aula\n\n
     Context:\n {context}?\n
     Question: \n{question}\n
 
@@ -54,7 +54,8 @@ def get_conversational_chain():
 
     model = ChatGoogleGenerativeAI(model="gemini-pro",
                                    client=genai,
-                                   temperature=0.7,
+                                   temperature=0.9,
+                                   max_output_tokens=8192,
                                    )
     prompt = PromptTemplate(template=prompt_template,
                             input_variables=["context", "question"])
@@ -80,7 +81,70 @@ def user_input(user_question):
     print(response)
     return response
 
+def main():
+    
+    st.logo(LOGO_URL_LARGE, link=None, icon_image=None)
+    st.set_page_config(
+        page_title="Assistente MSEP",
+        page_icon="🤖",
+    )
 
+    # Sidebar for uploading PDF files
+    st.image(LOGO_URL_LARGE)
+    with st.sidebar:
+        st.title("Menu:")
+        nomeCurso = st.text_input("Nome do Curso:", "")
+        nomeUC = st.text_input("Nome da Unidade Curricular:", "")
+        tipoDocumento = st.selectbox("Selecione o tipo de documento", ("Plano de Ensino", "Cronograma", "Plano de Aula"))
+        if(tipoDocumento=='Plano de Ensino'):   
+            qntSituacoesAprendizagem = st.number_input("Quantidade de estratégias de aprendizagem:",min_value=1)
+        estrategiaAprendizagem = st.selectbox("Selecione a estratégia de aprendizagem", ("Situação-Problema", "Estudo de Caso", "Projeto Integrador", "Projetos","Pesquisa Aplicada"))
+        pdf_docs = st.file_uploader("Carregue seus arquivos PDF e clique no botão Enviar e Processar", type='.pdf',accept_multiple_files=True)
+        if st.button("Enviar & Processar"):
+            with st.spinner("Processando..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Concluído")
+        if st.button("Gerar "+tipoDocumento):
+            with st.spinner("Processando..."):
+                 st.success("Concluído")
+
+    # Main content area for displaying chat messages
+    st.title("Assistente MSEP")
+    st.write("Bem vindo ao assistente do professor!")
+    st.sidebar.button('Limpar histórico do chat', on_click=clear_chat_history)
+
+    # Chat input
+    # Placeholder for chat messages
+
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [
+            {"role": "assistant", "content": "Carregue um plano de curso e elabore o documento desejado."}]
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
+
+    # Display chat messages and bot response
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Pensando..."):
+                response = user_input(prompt)
+                placeholder = st.empty()
+                full_response = ''
+                for item in response['output_text']:
+                    full_response += item
+                    placeholder.markdown(full_response)
+                placeholder.markdown(full_response)
+        if response is not None:
+            message = {"role": "assistant", "content": full_response}
+            st.session_state.messages.append(message)
 
 if __name__ == "__main__":
     main()

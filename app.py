cc9a39c8b8b4772bfc0d27655e523d534b30cb6c
Lucas Tadeu Monteiro Guedes Fernandes Salomao <lucas.salomao@sp.senai.br>
2024-07-02 13:36:55 -0300

    update

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import fitz  # Importa a biblioteca fitz para trabalhar com documentos PDF
 
 LOGO_VERMELHO = 'https://upload.wikimedia.org/wikipedia/commons/8/8c/SENAI_S%C3%A3o_Paulo_logo.png'  # URL do logotipo vermelho do SENAI São Paulo
 LOGO_AZUL = 'https://logodownload.org/wp-content/uploads/2019/08/senai-logo-1.png'  # URL do logotipo azul do SENAI São Paulo
+LOGO_SENAI=LOGO_AZUL
 
 generation_config = {
     "temperature": 1,  # Define a temperatura para a geração de texto (menor = mais previsível)
@@ -20,9 +21,11 @@ model = genai.GenerativeModel(
     generation_config=generation_config,  # Define a configuração de geração de texto
     # safety_settings = Adjust safety settings  # Ajusta as configurações de segurança (opcional)
     # See https://ai.google.dev/gemini-api/docs/safety-settings  # Link para a documentação das configurações de segurança
-    system_instruction="Você é um especialista em educação profissional, que trabalha no Senai São Paulo, que orienta os professores a como usar a metodologia senai de educação profissional para elaborar planos de ensino, cronogramas e planos de aula",  # Define a instrução do sistema para o modelo de linguagem
+    system_instruction="Você é um especialista em educação profissional, que trabalha no Senai São Paulo, que orienta os professores e instrutores a como usar a metodologia senai de educação profissional para elaborar planos de ensino, cronogramas e planos de aula. Responda somente a perguntas relacionas da MSEP, plano de curso, plano de ensino e outros documentos norteadores da prática pedagógica. Qualquer outro assunto, informe que você não está autorizado a dar informações, pois o seu propósito é unico e exclusivamente de ajudar os instrutores e não servir com um chatbot de propósito geral.",  # Define a instrução do sistema para o modelo de linguagem
 )
 
+apiKeyGoogleAiStudio='UNDEFINED'
+    
 def ler_arquivo_txt(nome_do_arquivo):
     """
     Lê o conteúdo de um arquivo de texto.
@@ -64,9 +67,9 @@ def clear_chat_history():
     Limpa o histórico de mensagens do chat.
     """
     st.session_state.messages = [
-        {"role": "assistant", "content": "Faça o upload da MSEP e do Plano de Curso desejado"}]
+        {"role": "assistant", "content": "Carregue um plano de curso e a MSEP e elabore o documento norteador de prática pedagógica de acordo com a Metodologia Senai de Educação Profissional."}]
 
-def get_gemini_reponse(prompt,raw_text):
+def get_gemini_reponse(prompt='',raw_text=''):
     """
     Obtém uma resposta do modelo Gemini.
 
@@ -121,11 +124,10 @@ def get_pdf_text_v2(pdf_docs):
 def main():
     """
     A função principal da aplicação.
-    """
+    """    
     if 'docs_raw' not in st.session_state:
         st.session_state.docs_raw = ''
-
-    st.logo(LOGO_AZUL, link=None, icon_image=None)  # Exibe o logotipo azul do SENAI
+    st.logo(LOGO_SENAI, link=None, icon_image=None)  # Exibe o logotipo azul do SENAI
 
     st.set_page_config(
         page_title="Assistente Virtual MSEP - Metodologia Senai de Educação Profissional",  # Define o título da página
@@ -136,19 +138,19 @@ def main():
     )
 
     # Sidebar for uploading PDF files
-    st.image(LOGO_AZUL, width=200)  # Exibe o logotipo azul na sidebar
+    st.image(LOGO_SENAI, width=200)  # Exibe o logotipo azul na sidebar
     with st.sidebar:
         # if (st.button("Ajuda?",href='hhhts:google.com'))
         st.link_button("Ajuda?",'https://sesisenaisp.sharepoint.com/:fl:/g/contentstorage/CSP_dffdcd74-ac6a-4a71-a09f-0ea299fe0911/EV9ykg9ssJhGrnX4TB58NyQBSsXBN2QKNoQP-pYjM9ucAQ?e=nVB4ya&nav=cz0lMkZjb250ZW50c3RvcmFnZSUyRkNTUF9kZmZkY2Q3NC1hYzZhLTRhNzEtYTA5Zi0wZWEyOTlmZTA5MTEmZD1iJTIxZE0zOTMycXNjVXFnbnc2aW1mNEpFVTFUeVBYQmF2QklrSzlOZFY3NW1CaWFlSTNNVWJBZlNaVmVlaXF0MUlaeSZmPTAxV1M1M0VCQzdPS0pBNjNGUVRCREs0NVBZSlFQSFlOWkUmYz0lMkYmYT1Mb29wQXBwJnA9JTQwZmx1aWR4JTJGbG9vcC1wYWdlLWNvbnRhaW5lciZ4PSU3QiUyMnclMjIlM0ElMjJUMFJUVUh4elpYTnBjMlZ1WVdsemNDNXphR0Z5WlhCdmFXNTBMbU52Ylh4aUlXUk5Nemt6TW5GelkxVnhaMjUzTm1sdFpqUktSVlV4VkhsUVdFSmhka0pKYTBzNVRtUldOelZ0UW1saFpVa3pUVlZpUVdaVFdsWmxaV2x4ZERGSldubDhNREZYVXpVelJVSkRUVTFQTXpNM1V6VlVRMFpITWtzMVZrMVBWMUZGTmxKWU5BJTNEJTNEJTIyJTJDJTIyaSUyMiUzQSUyMjFkN2M1ZjRiLWU0ZWQtNDBlMS04ZmE2LWM4YjQ4MjFkOTRmZCUyMiU3RA%3D%3D')
         st.title("Menu:")
-        apiKeyGoogleAiStudio = st.text_input("Chave de API Google AI Studio:", "", type='password')  # Campo de entrada para a chave API
+        apiKeyGoogleAiStudio = st.text_input("Chave de API Google AI Studio:", "", type='password',help="Obtenha sua chave de API em https://ai.google.dev/aistudio")  # Campo de entrada para a chave API
         nomeCurso = st.text_input("Nome do Curso:", "")  # Campo de entrada para o nome do curso
         nomeUC = st.text_input("Nome da Unidade Curricular:", "")  # Campo de entrada para o nome da unidade curricular
-        tipoDocumento = st.selectbox("Selecione o tipo de documento", ("Plano de Ensino", "Cronograma", "Plano de Aula"))  # Menu dropdown para selecionar o tipo de documento
+        tipoDocumento = st.selectbox("Selecione o tipo de documento:", ("Plano de Ensino", "Cronograma", "Plano de Aula"))  # Menu dropdown para selecionar o tipo de documento
         if(tipoDocumento=='Plano de Ensino'):
             qntSituacoesAprendizagem = st.number_input("Quantidade de situações de aprendizagem:", min_value=1)  # Campo de entrada numérico para a quantidade de estratégias de aprendizagem
-            estrategiaAprendizagem = st.selectbox("Selecione a estratégia de aprendizagem", ("Situação-Problema", "Estudo de Caso", "Projeto Integrador", "Projetos","Pesquisa Aplicada"))  # Menu dropdown para selecionar a estratégia de aprendizagem
-        pdf_docs = st.file_uploader("Carregue seus arquivos PDF e clique no botão Enviar e Processar", type='.pdf', accept_multiple_files=True, help='Faça o upload da MSEP e de um plano de curso que deseja elaborar os documentos de prática docente.')  # Carregador de arquivos PDF
+            estrategiaAprendizagem = st.selectbox("Selecione a estratégia de aprendizagem:", ("Situação-Problema", "Estudo de Caso", "Projeto Integrador", "Projetos","Pesquisa Aplicada"))  # Menu dropdown para selecionar a estratégia de aprendizagem
+        pdf_docs = st.file_uploader("Carregue seus arquivos PDF e clique no botão Enviar e Processar:", type='.pdf', accept_multiple_files=True, help='Faça o upload da MSEP e de um plano de curso que deseja elaborar os documentos de prática docente. Os documentos podem ser acessados em https://sesisenaisp.sharepoint.com/sites/NovaGED')  # Carregador de arquivos PDF
         if st.button("Processar documentos"):
             with st.spinner("Processando..."):
                 st.session_state.docs_raw = get_pdf_text(pdf_docs)  # Lê o texto dos arquivos PDF e armazena na sessão
@@ -162,14 +164,14 @@ def main():
 
     # Main content area for displaying chat messages
     st.title("Assistente Virtual MSEP - Metodologia Senai de Educação Profissional")  # Título da página
-    st.write("Bem vindo ao assistente virtual do professor para auxiliar a elaboração de documentos da prática pedagógica de acordo com a MSEP!")  # Mensagem de boas-vindas
+    st.write("Bem vindo ao assistente virtual do instrutor para auxiliar a elaboração de documentos da prática pedagógica de acordo com a MSEP!")  # Mensagem de boas-vindas
     st.sidebar.button('Limpar histórico do chat', on_click=clear_chat_history)  # Botão para limpar o histórico do chat
     st.sidebar.link_button("Reportar Bug",'https://forms.office.com/r/xLD92jjss7')
 
     # Chat input
     # Placeholder for chat messages
     if "messages" not in st.session_state.keys():
-        st.session_state.messages = [{"role": "assistant", "content": "Carregue um plano de curso e elabore o documento desejado de acordo com a MSEP."}]  # Mensagem inicial do assistente
+        st.session_state.messages = [{"role": "assistant", "content": "Carregue um plano de curso e a MSEP e elabore o documento norteador de prática pedagógica de acordo com a Metodologia Senai de Educação Profissional."}]  # Mensagem inicial do assistente
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
@@ -189,6 +191,7 @@ def main():
     if st.session_state.messages[-1]["role"] != "assistant":
         with st.chat_message("assistant"):
             with st.spinner("Pensando..."):
+                genai.configure(api_key=apiKeyGoogleAiStudio)
                 response = get_gemini_reponse(prompt, st.session_state.docs_raw)  # Obtém a resposta do modelo Gemini
                 placeholder = st.empty()  # Cria um placeholder para a resposta
                 placeholder.markdown(response)  # Exibe a resposta no placeholder
@@ -201,6 +204,24 @@ def main():
         if response is not None:
             message = {"role": "assistant", "content": response}
             st.session_state.messages.append(message)  # Adiciona a resposta ao histórico de mensagens
+
+    ##Testando input controlado
+    if prompt := st.chat_input(placeholder="Faça alguma pergunta ou solicitação"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
+
+    # Display chat messages and bot response
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Pensando..."):
+                genai.configure(api_key=apiKeyGoogleAiStudio)
+                response = get_gemini_reponse(prompt)
+                placeholder = st.empty()
+                placeholder.markdown(response)  # Exibe a resposta no placeholder
+        if response is not None:
+            message = {"role": "assistant", "content": response}
+            st.session_state.messages.append(message)
 
 if __name__ == "__main__":
     main()

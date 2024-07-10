db0ac7c735df793706950922cc08d45e6828b885
Lucas-Salomao <lucastadeusalomao@gmail.com>
2024-07-10 05:46:27 -0300

    update

--- promptPlanoEnsino.py
+++ promptPlanoEnsino.py
@@ -109,83 +109,180 @@
 # - A contextualização da estratégia de aprendizagem deve ser de acordo com o perfil profissional e trazer situações reais do mundo do trabalho."""
 
 promptPlanoDeEnsino="""
-Com base na Metodologia SENAI de Educação Profissional (MSEP), elabore um plano de ensino completo e de qualidade para o curso [Nome do Curso] da unidade curricular [Nome da Unidade Curricular] seguindo o seguinte modelo, formatado em markdown, e nada além disso:
+Com base na Metodologia SENAI de Educação Profissional (MSEP), elabore um plano de ensino completo e de qualidade para o curso [Nome do Curso] da unidade curricular [Nome da Unidade Curricular] seguindo o seguinte modelo formatado em markdown, exatamente como o modelo abaixo:
 
-# Plano de Ensino segundo a MSEP
+# Planejamento de Ensino segundo a MSEP
 
-**Título:** [Titulo da situação de aprendizagem]
+# Plano de ensino
+
+**Título:** [Titulo criativo da situação de aprendizagem]
 
 **Curso:** [Nome do curso]
 
 **Unidade Curricular (UC):** [Nome da unidade curricular]
 
-**Carga Horária total na UC:** [Carga horária do curso]
+**Carga Horária total na UC:** [Carga horária total do curso]
 
 **Nº de aulas previstas para desenvolver a situação de aprendizagem:** [Número de aulas]
 
 **Carga horária prevista para o desenvolvimento da Situação de Aprendizagem:** [Carga horária prevista para o desenvolvimento da Situação de Aprendizagem]
 
-**Objetivo da Unidade Curricular:** [Objetivo geral da unidade curricular]
+**Objetivo da Unidade Curricular:** [Objetivo geral da unidade curricular de acordo com o plano de curso]
 
 **Modalidade de ensino:** [Presencial, EAD ou Híbrida]
 
 ## Capacidades a serem desenvolvidas:
 
-- Nesse campo, precisamos descrever todas as capacidades que serão desenvolvidas na Situação de Aprendizagem (sejam básicas, técnicas e socioemocionais).
-- Ao selecionarmos as capacidades que serão desenvolvidas, precisamos cuidar da gradualidade das capacidades, trabalhando com propostas que contemplem capacidades de menor complexidade para maior complexidade.
-- Importante: não podemos alterar as capacidades previstas no plano de curso.
-- Exemplo: do verbo identificar para o verbo configurar, temos uma diferença grande na complexidade da capacidade preterida.
+[
+    - Nesse campo, precisamos descrever todas as capacidades que serão desenvolvidas na Situação de Aprendizagem (sejam básicas, técnicas e socioemocionais).
+    - Ao selecionarmos as capacidades que serão desenvolvidas, precisamos cuidar da gradualidade das capacidades, trabalhando com propostas que contemplem capacidades de menor complexidade para maior complexidade.
+    - Importante: não podemos alterar as capacidades previstas no plano de curso.
+    - Exemplo: do verbo identificar para o verbo configurar, temos uma diferença grande na complexidade da capacidade preterida.
+]
 
 ### Capacidades Básicas:
 
-[Listas as capacidades básicas de acordo com o plano de curso, necessárias para o desenvolvimento da situação de aprendizagem. As capacidades básicas pertencem somente aos módulos básicos.]
+[Se a unidade curricular pertencer ao módulo básico, listar as capacidades básicas de acordo com o plano de curso, necessárias para o desenvolvimento da situação de aprendizagem.] 
 
 ### Capacidades Técnicas:
 
-[Listas as capacidades técnicas de acordo com o plano de curso, necessárias para o desenvolvimento da situação de aprendizagem. As capacidades técnicas pertencem somente aos módulos específicos.]
+[Se a unidade curricular pertencer ao módulo especifico, listar as capacidades técnicas de acordo com o plano de curso, necessárias para o desenvolvimento da situação de aprendizagem.]
 
 ### Capacidades Socioemocionais:
 
-[Listas as capacidades socioemocionais de acordo com o plano de curso, necessárias para o desenvolvimento da situação de aprendizagem.]
+[Listar as capacidades socioemocionais de acordo com o plano de curso, necessárias para o desenvolvimento da situação de aprendizagem.]
 
 ## Conhecimentos:
 
-[Conhecimentos de acordo com o plano de curso e unidade curricular necessários para o desenvolvimento da situação de aprendizagem.
+[
+    - Conhecimentos de acordo com o plano de curso e unidade curricular necessários para o desenvolvimento da situação de aprendizagem.]
+    - Listar os conhecimentos exatamente como aparecem no plano de curso, incluindo a numeração exata de cada item, da mesma forma que apresentada no plano de curso.
+]
 
-[Listar os conhecimentos exatamente como aparecem no plano de curso, incluindo a numeração exata de cada item, da mesma forma que apresentada no plano de curso.]
-
-## Estratégia de  aprendizagem desafiadora:
-
-[Indicar o tipo de estratégia de aprendizagem escolhida]
+## Estratégia de  aprendizagem desafiadora: [Indicar o tipo de estratégia de aprendizagem escolhida em itálico]
 
 ### Contextualização:
 
-[Descrição da contextualização da Situação de Aprendizagem]
-
-- Nesse campo, a abordagem contextualizada é pensada para construir cenários reais da situação de trabalho que o aluno vai enfrentar. Por isso, é importante que o aluno encontre máquinas, equipamentos, instrumentos, ferramentas, materiais e condições de trabalho bem semelhantes às dos ambientes em que vai atuar.
-- Recomendamos abordar a área tecnológica da empresa, nº de funcionários, perfil do cliente interno (técnico ou gestor), do cliente externo, explanar sobre o tipo de serviço prestado pela empresa, dados atuais versus dados pretendidos com a implementação do trabalho proposto, visando ampliar o repertório do aluno.
-- Para planejarmos a SA(Situação de Aprendizagem), a MSEP sugere que tenhamos respostas para 5 perguntas:
-    - O que? Para que? Como? Com o que? Onde?
-- Com as respostas às essas perguntas, precisamos considerar 3 requisitos para o planejamento:
-    - Mobilização
-    - Resolução de problemas com tomada de decisão
-    - Máximo de circulação de informações possíveis.
-- Estimulamos as competências de visão sistêmica e de criatividade com a Situação de Aprendizagem?
-- Indicar a necessidade de inclusão de figuras, esquemas, desenhos, leiaute, formulários, etc, para complementar a contextualização.
+[
+    - Descrição da contextualização da Situação de Aprendizagem
+    - Nesse campo, a abordagem contextualizada é pensada para construir cenários reais da situação de trabalho que o aluno vai enfrentar. Por isso, é importante que o aluno encontre máquinas, equipamentos, instrumentos, ferramentas, materiais e condições de trabalho bem semelhantes às dos ambientes em que vai atuar.
+    - Recomendamos abordar a área tecnológica da empresa, nº de funcionários, perfil do cliente interno (técnico ou gestor), do cliente externo, explanar sobre o tipo de serviço prestado pela empresa, dados atuais versus dados pretendidos com a implementação do trabalho proposto, visando ampliar o repertório do aluno.
+    - Para planejarmos a SA(Situação de Aprendizagem), a MSEP sugere que tenhamos respostas para 5 perguntas:
+        - O que? Para que? Como? Com o que? Onde?
+    - Com as respostas às essas perguntas, precisamos considerar 3 requisitos para o planejamento:
+        - Mobilização
+        - Resolução de problemas com tomada de decisão
+        - Máximo de circulação de informações possíveis.
+    - Estimulamos as competências de visão sistêmica e de criatividade com a Situação de Aprendizagem?
+    - Indicar a necessidade de inclusão de figuras, esquemas, desenhos, leiaute, formulários, etc, para complementar a sitação de aprendizagem.
+]
 
 ## Desafio:
 
-[Descrição do desafio proposto na Situação de Aprendizagem]
-
-- A MSEP recomenda que o desafio da SA precisa ser diferente do que o aluno já realizou, mas isso não significa que precisa ser inédito.
-- Precisa ser fruto de muita reflexão, tomada de decisão, da realização de uma ou mais atividades. Precisamos ficar atentos ao que chamamos de “resposta pronta”.
-- Quando as capacidades e conhecimentos requerem análise de dados, comparação ou correlação de soluções alternativas, como a escola propôs, é interessante abordar na perspectiva do estudo de caso e os alunos precisam trazer soluções de sucesso ou insucesso.
-- Indicar a necessidade de inclusão de figuras, esquemas, desenhos, leiaute, formulários, etc, para complementar o desafio.
+[
+    - Descrição do desafio proposto na Situação de Aprendizagem
+    - A MSEP recomenda que o desafio da SA precisa ser diferente do que o aluno já realizou, mas isso não significa que precisa ser inédito.
+    - Precisa ser fruto de muita reflexão, tomada de decisão, da realização de uma ou mais atividades. Precisamos ficar atentos ao que chamamos de “resposta pronta”.
+    - Quando as capacidades e conhecimentos requerem análise de dados, comparação ou correlação de soluções alternativas, como a escola propôs, é interessante abordar na perspectiva do estudo de caso e os alunos precisam trazer soluções de sucesso ou insucesso.
+]
 
 ### Resultados Esperados:
 
-[Descrição detalhada dos resultados esperados]
+[
+    - Descrição detalhada dos resultados esperados
+    - A MSEP nos orienta que, ao redigir a estratégia de aprendizagem desafiadora, o docente deve informar claramente o que espera do aluno como um produto final: relatório, trabalho escrito, projeto, protótipo, produto (bem ou serviço), maquete, softwares, vídeos, manuais, pareceres, leiaute, entre outros. Esses resultados devem ser adequados e proporcionais à contextualização e ao nível de exigência do desafio proposto. (p.138 MSEP)
+]
 
-- A MSEP nos orienta que, ao redigir a estratégia de aprendizagem desafiadora, o docente deve informar claramente o que espera do aluno como um produto final: relatório, trabalho escrito, projeto, protótipo, produto (bem ou serviço), maquete, softwares, vídeos, manuais, pareceres, leiaute, entre outros. Esses resultados devem ser adequados e proporcionais à contextualização e ao nível de exigência do desafio proposto. (p.138 MSEP)
+# Plano de Aula
+
+## Instrumento de Registro
+
+**Nome do aluno:** [Nome do Aluno]
+**Turma:** [Turma]
+[
+    -Tabela contendo as colunas:
+        - Capacidades básicas ou técnicas e socioemocionais:
+            [
+                - Capacidades necessárias para o desenvolvimento da situação de aprendizagem, extraídas da unidade curricular do plano de curso.
+                - As capacidades básicas (B), técnicas (T) ou socioemocionais (S) devem ser exatamente as mesmas contidas no plano de curso.
+            ]
+        - Critérios de Avaliação:
+            [
+                - Critérios Críticos (C) e Critérios Desejáveis (D) necessários para avaliar as capacidades no desenvolvimento da situação de aprendizagem.
+                - Elaborar exatamente 2 critérios críticos (C) e 2 critérios desejáveis (D) para cada uma das capacidades.
+                - Para a formulação dos critérios de avaliação, a MSEP recomenda algumas características estruturais essenciais. São elas: objetividade, mensuração, granularidade e transparência.
+            ]
+        - Resultado:
+            [
+                - deixar em branco para ser prenchido posteriormente.
+            ]
+]
+    Legenda:A=Atingiu/N=Não Atingiu
+
+## Plano de Aula
+
+    - Tabela contendo as colunas:
+        - Nº horas/aula e data:
+            [Ex. X aulas XX/XX/20XX]
+        - Capacidades a serem desenvolvidas:
+            [ Capacidades básicas (B), técnicas (T) e socioemocionais (S) de acordo com o plano de curso e selecionadas de acordo com a situação de aprendizagem a ser desenvolvida na aula.]
+        - Conhecimentos relacionados:
+            [ Conhecimentos selecionados de acordo com o plano de curso para o desenvolvimento da situação de aprendizagem na aula.]
+        - Estratégias de ensino:
+            [ Definir a mais adequada para o deenvolvimento da situação de aprendizagem na aula]
+            [   - Exposição Dialogada:
+                    - Apresentação de conteúdos, mas com foco no diálogo e participação ativa dos alunos.
+                    - Utilização de recursos como imagens, vídeos e problematizações para tornar a aula mais dinâmica.
+                    - Abertura para questionamentos, reflexões e críticas por parte dos alunos.
+                - Atividade Prática:
+                    - "Aprender fazendo": colocar o conhecimento em prática através da execução de tarefas e produção de resultados concretos.
+                    - Promover a articulação entre teoria e prática na resolução de desafios.
+                - Trabalho em Grupo:
+                    - Fomentar a colaboração entre os alunos e a construção coletiva de conhecimento.
+                    - Desenvolver capacidades socioemocionais como comunicação, colaboração e resolução de conflitos.
+                - Dinâmica de Grupo:
+                    - Promover a interação e integração entre os alunos de forma lúdica e participativa.
+                    - Pode ser utilizada para diversos fins, como introdução de temas, levantamento de conhecimentos prévios e avaliação da aprendizagem.
+                - Visita Técnica:
+                    - Ampliar os espaços de aprendizagem, levando os alunos para contextos reais de trabalho.
+                    - Observar processos produtivos, funcionamento de equipamentos e execução de atividades profissionais.
+                - Ensaio Tecnológico:
+                    - Verificar padrões de qualidade e funcionalidade de produtos e protótipos em ambientes controlados como laboratórios.
+                    - Realização de testes e análises para aplicar o conhecimento teórico na prática.
+                - Workshop:
+                    - Oficina prática com foco no aprofundamento de um tema específico.
+                    - Combinação de exposição, aplicação prática, debate e fechamento, com intensa participação dos alunos.
+                - Seminário:
+                    - Apresentação e debate sobre temas específicos por especialistas ou pelos próprios alunos.
+                    - Exige pesquisa prévia e organização de um roteiro para garantir a qualidade da apresentação.
+                - Painel Temático:
+                    - Debate entre especialistas ou grupos de alunos sobre conclusões de estudos e diferentes pontos de vista.
+                    - Conduzido por um moderador que garante a organização e o foco no tema.
+                - Gameficação:
+                    - Utilização de jogos e elementos lúdicos para promover a aprendizagem de forma divertida e engajadora.
+                    - Estimula o desenvolvimento de capacidades e a interação entre alunos e docente.
+                - Sala de Aula Invertida:
+                    - Inversão da lógica tradicional da aula: o aluno estuda os conceitos básicos previamente em casa e aprofunda a aprendizagem em sala com atividades práticas e interativas.
+                - Design Thinking:
+                    - Abordagem para resolução de problemas com foco no ser humano e na geração de soluções inovadoras.
+                    - Etapas de imersão, ideação e prototipagem para desenvolver soluções criativas e eficazes.
+            ]
+        - Instrumentos de Avaliação:
+            [ Listar como será avaliado as capacidades a serem desenvolvidas na aula.]
+        - Recursos e ambientes pedagógicos:
+            [ Computador, internet, notion, Microsoft Teams, Microsoft Learn, Plataforma de Gamificação Quizziz, Forms, Mentimeter, Kahoot, entre outras ferramentas ou recursos.]
+        -Critérios de Avaliação:
+            [ Listar todos os critérios de avaliação da tabela de instrumento de registro, sendo critérios críticos (C) e critérios desejáveis (D), necessários para o desenvolvimento da situação de aprendizagem na aula.]
+    
+    - Referências:
+            [Listar livros, apostilas, sites, blogs, etc, formatados de acordo com a ABNT.]
+            
+**Observações:**
+
+- Utilize a linguagem clara e objetiva.
+- Inclua exemplos e informações relevantes para cada item.
+- Mantenha a coerência entre as diferentes etapas do plano de ensino.
+- Siga as diretrizes da MSEP.
+- A contextualização da estratégia de aprendizagem deve ser de acordo com o perfil profissional e trazer situações reais do mundo do trabalho.
 
 """
